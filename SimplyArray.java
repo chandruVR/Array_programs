f6981e7205f7fb2cee774a7fefeb92fff1eea8cb
chandruVR <chandruchaanu@gmail.com>
2023-03-08 23:28:16 +0530

    first array

---
+++ SimplyArray.java
@@ -0,0 +1,20 @@
+package Array_Programs;
+
+public class SimplyArray {
+
+	public static void main(String[] args) {
+		// dierct initialization 
+		
+		  int arr[] = { 1,6,7}; // index 0 1 2 . 
+		
+	    int list[] = new int[0]; 
+	     list[0] =1;
+	     // list[1] =2;
+	    // list[2] =3; error index out of 
+	   //  System.out.println(list[0]); 
+	 
+   
+
+	}
+
+}
